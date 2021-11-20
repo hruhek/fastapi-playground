8c115addfbb8dc6c36b49407e0d57fd3264f45e5
Hrvoje Ruhek <trulija@gmail.com>
2021-11-20 15:06:30 +0100

    Reformated demo example.

--- demo/main.py
+++ demo/main.py
@@ -21,6 +21,7 @@ def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.put("/item/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
