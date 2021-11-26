255d9804a3e74add1fc4155e6636efec4445d5f1
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2021-11-26 19:31:26 +0100

    Query param examples.

--- tutorial/main.py
+++ tutorial/main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 from fastapi import FastAPI
 
@@ -8,6 +9,8 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 app = FastAPI()
 
@@ -31,3 +34,29 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/items/")
+async def read_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+async def update_item(item, q, short):
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+
+
+@app.get("/item/{item_id}")
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    await update_item(item, q, short)
+    return item
+
+
+@app.get("/user/{user_id}/item/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    await update_item(item, q, short)
+    return item
